d8805721acd7f0b2817804961401938bba839ec2
Aniket Shelke <aniketshelke8487@gmail.com>
2025-10-22 06:04:49 +0000

    error handling

--- src/app.js
+++ src/app.js
@@ -1,15 +1,34 @@
 const express = require("express");
 const app = express();
 
-app.use("/user", (req, res, next) => {
-  console.log("Handling the request");
-  // res.send("response");
-  next();
+const { adminAuth, userAuth } = require("./middlewares/auth");
+app.use("/admin", adminAuth);
+
+app.get("/user", userAuth, (req, res) => {
+  res.send("user get data");
 });
 
-app.use("/user", (req, res, next) => {
-  console.log("Handling the request");
-  res.send("response2");
+app.get("/admin/getdata", (req, res) => {
+  res.send("data recieved");
+});
+
+app.get("/admin/deleteuser", (req, res) => {
+  res.send("user is deleted");
+});
+
+app.get("/getdata", (req, res) => {
+  try {
+    throw new Error("dagadf");
+    res.send("user sent data");
+  } catch (err) {
+    res.status(500).send("something went wrong..");
+  }
+});
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log("err");
+    res.status(500).send("something went wrong..");
+  }
 });
 
 app.listen(3000, () => {
